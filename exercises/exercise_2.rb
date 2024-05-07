4a4839f86d136825eb57370dfc14558d117e8a3d
LHY927 <liuhaiyue0927@gmail.com>
2024-05-07 11:15:24 +0200

    update exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Load the first store by ID
+@store1 = Store.find(1)
+
+# Load the second store by ID
+@store2 = Store.find(2)
+
+# Update the first store's name and save the change
+@store1.update(name: 'New Burnaby')
+
+# Optionally, print to verify
+puts @store1.name
+puts @store2.name
