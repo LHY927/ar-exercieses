91e97da82cb37c1cbebcb3b3d54e8a88b8fcbfbf
LHY927 <liuhaiyue0927@gmail.com>
2024-05-07 11:26:22 +0200

    update exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the third store by ID
+@store3 = Store.find(3)
+
+# Delete the store from the database
+@store3.destroy
+
+# Verify the deletion by outputting the current count of stores
+puts Store.count
