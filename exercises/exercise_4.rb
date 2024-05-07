22abb2d59e95c3116796457f020a5cfbd1e1c67a
LHY927 <liuhaiyue0927@gmail.com>
2024-05-07 11:27:51 +0200

    update exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Create additional stores
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Output each men's store's name and annual revenue
+@mens_stores.each do |store|
+  puts "#{store.name} - Annual Revenue: #{store.annual_revenue}"
+end
+
+# Fetch stores that carry women's apparel and have less than $1M in annual revenue
+@womens_stores_under_1M = Store.where(womens_apparel: true).where('annual_revenue < ?', 1000000)
+
+# Optionally, to display these women's apparel stores:
+@womens_stores_under_1M.each do |store|
+  puts "#{store.name} - Annual Revenue: #{store.annual_revenue}"
+end
